b7059822d27633d249540c9a7543ef4852118f25
Bruemor <arymer1@gmail.com>
2016-02-22 16:50:51 -0500

    commit dealing with row's in my html

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,4 +2,23 @@ console.log("Hello World!");
 
 var $ = require('jquery');
 
-$('li').hide();
+$('li').hide(); //this is to hide my list items until I need them.
+
+$('ul').hover( //Use .hover in order to add a class that I can then style within css.
+  function(){
+    $(this).addClass('hover-background');
+  }, function(){
+    $(this).removeClass('hover-background');
+  }
+);
+
+// $('ul').click(function{
+//   $('li').slideDown( 'slow', function(){
+//
+//   });
+// });
+$('.my-files').click(function() {
+  $( '#local' ).slideDown( "slow", function() {
+    // Animation complete.
+  });
+});
